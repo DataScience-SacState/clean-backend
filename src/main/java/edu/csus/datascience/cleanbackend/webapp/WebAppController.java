a10f75adc248ad7960641637b115c4d755533639
MatthewMerrill <MattMerr47@gmail.com>
2016-04-09 20:58:21 -0700

    lol

--- src/main/java/edu/csus/datascience/cleanbackend/webapp/WebAppController.java
+++ src/main/java/edu/csus/datascience/cleanbackend/webapp/WebAppController.java
@@ -1,11 +1,12 @@
 package edu.csus.datascience.cleanbackend.webapp;
 
+import com.auth0.Auth0User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.support.RequestContext;
 
-import javax.xml.ws.ResponseWrapper;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by merrillm on 4/9/16.
@@ -14,10 +15,17 @@ import javax.xml.ws.ResponseWrapper;
 public class WebAppController {
 
     @RequestMapping("/webapp")
-    public String root(Model model) {
+    public String root(Model model, HttpServletRequest req, HttpServletResponse res) {
         System.out.println("Hi!");
-        System.out.println(model.asMap());
-        return "index";
+        System.out.println(Auth0User.get(req));
+
+        try {
+            model.addAttribute("name", Auth0User.get(req).getName());
+        } catch (Exception e) {
+            return "index";
+        }
+
+        return "greeting";
 
     }
 
